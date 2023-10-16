12552d5352348e7ebf7f358b565c441d8e064360
Saul Vieira <vieira_saul@yahoo.com.br>
2023-10-16 10:08:01 -0300

    feat: change pt to default language

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import createMiddleware from 'next-intl/middleware';
  
 export default createMiddleware({
   locales: ['en', 'pt', 'es'],
-  defaultLocale: 'en'
+  defaultLocale: 'pt'
 });
  
 export const config = {
