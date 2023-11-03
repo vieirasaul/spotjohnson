2c636ba4f8beef89910660716060d922dc322b0e
Saul Vieira <vieira_saul@yahoo.com.br>
2023-11-03 18:35:00 -0300

    feat: add main title and subtitle

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,26 @@
+'use client';
+
+import { Ubuntu } from 'next/font/google'
 import styles from './page.module.css'
+import { useTranslations } from 'next-intl';
+import Container from '../components/Container'
+
+const ubuntu = Ubuntu({
+  subsets: ['latin'],
+  weight: ['400', '500', '700']
+})
 
 export default function Home() {
+  const t = useTranslations('main');
+
   return (
-    <main>      
+    <main className={`${ubuntu.className} ${styles.Main}`}>
+      <Container>
+        <div className={styles.Wrapper}>
+          <h1>{t('title')}</h1>
+          <h2>{t('subtitle')}</h2>
+        </div>
+      </Container>
     </main>
   )
 }
